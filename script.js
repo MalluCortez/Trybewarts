5642a1f40f4ce01a523dd684b232ce89c976a2b3
MalluCortez <maria.santana@outlook.com.br>
2022-09-14 20:22:20 -0300

    requisito 3 finalizado

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const email = document.getElementById("email")
+const senha = document.getElementById("password")
+const buttonPermission = document.getElementById("buttonPermission");
+
+function permission() {
+  if (email.value === "tryber@teste.com" && senha.value === "123456") {
+    alert("Olá, Tryber!")
+  } else {
+    alert("Email ou senha inválidos.")
+  }
+}
+
+buttonPermission.addEventListener("click", permission)
